e4375f43e8232893a1968cbfaa6dca9724d9634b
FAMASoon <famasoon@gmail.com>
2023-08-11 12:20:24 +0900

    use jwt token

--- nest-admin/src/auth/auth.controller.ts
+++ nest-admin/src/auth/auth.controller.ts
@@ -1,11 +1,16 @@
-import { BadRequestException, Body, Controller, NotFoundException, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, NotFoundException, Post, Res } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
 import { RegisterDto } from './models/register.dto';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
 
 @Controller()
 export class AuthController {
-  constructor(private userService: UserService) {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService
+  ) {
 
   }
 
@@ -26,7 +31,8 @@ export class AuthController {
   @Post('login')
   async login(
     @Body('email') email: string,
-    @Body('password') password: string
+    @Body('password') password: string,
+    @Res({ passthrough: true }) response: Response
   ) {
     const user = await this.userService.findOne({ email })
     if (!user) {
@@ -35,6 +41,11 @@ export class AuthController {
     if (!await bcrypt.compare(password, user.password)) {
       throw new BadRequestException('Invalid credentials')
     }
+
+    const jwt = await this.jwtService.signAsync({ id: user.id })
+
+    response.cookie('jwt', jwt, { httpOnly: true })
+
     return user
   }
 }
--- nest-admin/src/auth/auth.module.ts
+++ nest-admin/src/auth/auth.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    UserModule
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: 'secret',
+      signOptions: { expiresIn: '1d' },
+    }),
   ],
   controllers: [AuthController]
 })
