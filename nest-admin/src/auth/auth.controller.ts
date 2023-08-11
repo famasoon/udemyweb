8d98f71e95c4b1930cb3050b1356170b3b464fda
FAMASoon <famasoon@gmail.com>
2023-08-11 10:38:33 +0900

    encrypted password

--- nest-admin/src/auth/auth.controller.ts
+++ nest-admin/src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Controller()
 export class AuthController {
@@ -9,6 +10,12 @@ export class AuthController {
 
   @Post('register')
   async register(@Body() body) {
-    return this.userService.create(body)
+    const hashedPassword = await bcrypt.hash(body.password, 12)
+    return this.userService.create({
+      first_name: body.first_name,
+      last_name: body.last_name,
+      email: body.email,
+      password: hashedPassword
+    })
   }
 }
