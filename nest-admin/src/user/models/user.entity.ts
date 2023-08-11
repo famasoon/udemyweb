de856b740c7788fbb6527c3f0431dcf5c510af03
FAMASoon <famasoon@gmail.com>
2023-08-11 10:33:40 +0900

    Create User method

--- nest-admin/src/user/models/user.entity.ts
+++ nest-admin/src/user/models/user.entity.ts
@@ -6,10 +6,10 @@ export class User {
   id: number;
 
   @Column()
-  fistName: string;
+  first_name: string;
 
   @Column()
-  lastName: string;
+  last_name: string;
 
   @Column({ unique: true })
   email: string;
