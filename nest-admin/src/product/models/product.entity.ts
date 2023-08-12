7076ee169bec26f52ea5b8c182573015ae103537
FAMASoon <famasoon@gmail.com>
2023-08-12 13:02:03 +0900

    Created product

---
+++ nest-admin/src/product/models/product.entity.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number
+  @Column()
+  title: string
+  @Column()
+  description: string
+  @Column()
+  image: string
+  @Column()
+  price: number
+}
