1a12735f89d71397c33b0258641efc02fc89f395
FAMASoon <famasoon@gmail.com>
2023-08-12 14:22:50 +0900

    feat: create order

---
+++ nest-admin/src/order/order.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { OrderService } from './order.service';
+import { AuthGuard } from 'src/auth/auth/auth.guard';
+
+@UseGuards(AuthGuard)
+@Controller()
+export class OrderController {
+  constructor(private orderService: OrderService) { }
+  @Get('orders')
+  async all(@Query('page') page = 1) {
+    return this.orderService.paginate(page, ['order_items'])
+  }
+}
